b741ef48ce5db71660c2ba00fce623238b318a63
Elina <elinabeishenalieva.eu@gmail.com>
2022-05-10 11:51:01 +0600

    first

---
+++ hw/HW1/main.js
@@ -0,0 +1,9 @@
+const itnInput = document.querySelector(".itnInput")
+const itnClick = document.querySelector(".itnClick")
+const itnResult = document.querySelector(".itnResult")
+
+itnClick.addEventListener("click", () => {
+
+})
+
+const itnValidate = / 1/
