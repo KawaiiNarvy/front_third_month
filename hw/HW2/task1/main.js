d0428f4239747dcc10b163a88e1888017f61402f
Elina <elinabeishenalieva.eu@gmail.com>
2022-05-15 01:14:54 +0600

    second hw

---
+++ hw/HW2/task1/main.js
@@ -0,0 +1,35 @@
+const block = document.querySelector(".block")
+
+let x = 0;
+let y = 0;
+
+const motion = () => {
+    if (x < 450 && y == 0) {
+        x += 10
+        block.style.left = `${x}px`;
+        setTimeout(() => {
+            motion()
+        }, 100)
+    } else if (x == 450 && y < 450) {
+        y += 10
+        block.style.top = `${y}px`
+        setTimeout(() => {
+            motion()
+        }, 100)
+    } else if (x > 0 && y == 450) {
+        x -= 10
+        block.style.left = `${x}px`
+        setTimeout(() => {
+            motion()
+        }, 100)
+    } else if (x == 0 && y > 0) {
+        y -= 10
+        block.style.top = `${y}px`
+        setTimeout(() => {
+            motion()
+        }, 100)
+    } else {
+        return;
+    }
+}
+motion()
---
+++ hw/HW2/task2/main.js
@@ -0,0 +1,6 @@
+let sec = 0
+
+setInterval(() => {
+    sec++
+    console.log(sec)
+}, 1000)
